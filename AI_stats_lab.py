5a155a4c4f76fb3fd30e251aebbffbef460ddc3f
FAIZA AMEEN <faizaameen001@gmail.com>
2026-04-17 11:55:50 +0500

    2024-DS-20

--- AI_stats_lab.py
+++ AI_stats_lab.py
@@ -21,7 +21,31 @@ def uniform_analysis(a, n_samples=10000):
         transformed_variance
     )
     """
-    pass
+
+    theoretical_mean = a / 2
+    theoretical_variance = (a ** 2) / 12
+
+    samples = np.random.uniform(0, a, n_samples)
+    
+    sample_mean = np.mean(samples)
+    sample_variance = np.var(samples)
+
+    mean_error = abs(sample_mean - theoretical_mean)
+    variance_error = abs(sample_variance - theoretical_variance)
+
+    transformed_mean = 2 * theoretical_mean + 1
+    transformed_variance = (2 ** 2) * theoretical_variance
+
+    return (
+        theoretical_mean,
+        theoretical_variance,
+        sample_mean,
+        sample_variance,
+        mean_error,
+        variance_error,
+        transformed_mean,
+        transformed_variance
+    )
 
 
 if __name__ == "__main__":
